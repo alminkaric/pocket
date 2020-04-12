c6079b6500002866b20eb134cf9651b126c04bc8
Almin Karic <almin_k@live.com>
2020-04-12 11:57:53 +0200

    Add typescript support

--- app/javascript/packs/hello_react.tsx
+++ app/javascript/packs/hello_react.tsx
@@ -4,19 +4,12 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
-const Hello = props => (
+interface HelloProps { name: string; }
+
+const Hello = (props: HelloProps) => (
   <div>Hello {props.name}!</div>
 )
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
